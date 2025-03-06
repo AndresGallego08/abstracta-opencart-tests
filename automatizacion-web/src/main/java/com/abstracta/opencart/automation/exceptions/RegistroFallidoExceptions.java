0d076e24451dc4a64c281f6b0968cfa6a527482d
unknown <andres.gallego@softgic.co>
2025-03-06 10:28:01 -0500

    Commit inicial con estructura base del proyecto

---
+++ automatizacion-web/src/main/java/com/abstracta/opencart/automation/exceptions/RegistroFallidoExceptions.java
@@ -0,0 +1,9 @@
+package com.abstracta.opencart.automation.exceptions;
+
+public class RegistroFallidoExceptions extends AssertionError{
+
+    public RegistroFallidoExceptions(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
